5954af5a74bd33619381717a7a5a71fe6ada98c1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-04-26 23:48:35 +0200

    Add binairy to generate client

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-$generator = new \ApiClients\Tools\OpenApiClientGenerator\Generator('https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.yaml');
-$generator->generate('ApiClients\Github\\', 'generated');

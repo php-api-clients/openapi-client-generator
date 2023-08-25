60e8dd2a09d9cf56d9c3e35e11f0fe705f38d274
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-08-25 08:52:48 +0200

    CS fixes

--- src/Output/Error.php
+++ src/Output/Error.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace ApiClients\Tools\OpenApiClientGenerator\Output;
 
-use ApiClients\Tools\OpenApiClientGenerator\Output\Status\ANSI;
-use ApiClients\Tools\OpenApiClientGenerator\Output\Status\Simple;
-use ApiClients\Tools\OpenApiClientGenerator\Output\Status\Step;
 use OndraM\CiDetector\CiDetector;
+use Throwable;
+
+use function file_put_contents;
+use function getenv;
 use function Termwind\render;
+
+use const FILE_APPEND;
 
 final readonly class Error
 {
-    public static function display(\Throwable $throwable)
+    public static function display(Throwable $throwable): void
     {
         render('<div>
             <div class="px-1 bg-red-600">ERROR</div>
@@ -21,8 +24,10 @@ final readonly class Error
             </em>
         </div>');
 
-        if ((new CiDetector())->detect()->getCiName() === CiDetector::CI_GITHUB_ACTIONS) {
-            file_put_contents(getenv('GITHUB_STEP_SUMMARY'), "### ⚠️ Error ⚠️\n```" . $throwable->getMessage() . "```\n", FILE_APPEND);
+        if ((new CiDetector())->detect()->getCiName() !== CiDetector::CI_GITHUB_ACTIONS) {
+            return;
         }
+
+        file_put_contents(getenv('GITHUB_STEP_SUMMARY'), "### ⚠️ Error ⚠️\n```" . $throwable->getMessage() . "```\n", FILE_APPEND);
     }
 }
